4054d51d4bcd01ca865b758b51c45c274ecbddf3
arthur <arthurfleck8@proton.me>
2024-03-25 23:55:23 +0100

    but also a rabbit hole ^^

---
+++ qtile/.config/qtile/autostart.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash 
+
+#COLORSCHEME=DoomOne
+
+
+### AUTOSTART PROGRAMS ###
+#lxsession &
+"$HOME"/.screenlayout/arandr-config-viewsonic.sh &
+set_random_wallpaper
+sleep 1
+#picom --daemon &
+/usr/bin/emacs --daemon &
+nm-applet &
+blueman-applet &
+megasync &
+
+#"$HOME"/.screenlayout/layout.sh &
+#sleep 1
+#conky -c "$HOME"/.config/conky/qtile/01/"$COLORSCHEME".conf || echo "Couldn't start conky."
+
+### UNCOMMENT ONLY ONE OF THE FOLLOWING THREE OPTIONS! ###
+# 1. Uncomment to restore last saved wallpaper
+#xargs xwallpaper --stretch < ~/.cache/wall &
+# 2. Uncomment to set a random wallpaper on login
+# find /usr/share/backgrounds/dtos-backgrounds/ -type f | shuf -n 1 | xargs xwallpaper --stretch &
+# 3. Uncomment to set wallpaper with nitrogen
+# nitrogen --restore &
