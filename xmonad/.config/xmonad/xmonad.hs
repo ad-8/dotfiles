b7ff7c4605e1e8cdaffb2cb3580790cf06fc2389
arthur <arthurfleck8@proton.me>
2024-03-28 09:42:22 +0100

    change mod key and add some keybindings

--- xmonad/.config/xmonad/xmonad.hs
+++ xmonad/.config/xmonad/xmonad.hs
@@ -4,6 +4,15 @@ import XMonad.Util.EZConfig
 
 -- a basic configuration — which is the same as the default config
 -- default config: https://github.com/xmonad/xmonad/blob/master/src/XMonad/Config.hs
+-- Do note that it is not advised to copy that file and use it as the basis of your configuration, as you won’t notice when a default changes!
+
 main :: IO ()
-main = xmonad def
+main = xmonad $ def
+    { modMask = mod4Mask  -- Rebind Mod to the Super key
+    }
+  `additionalKeysP`
+    [ ("M-S-z", spawn "xscreensaver-command -lock")
+    , ("M-C-s", unGrab *> spawn "scrot -s"        )
+    , ("M-f"  , spawn "firefox"                   )
+    ]
 
