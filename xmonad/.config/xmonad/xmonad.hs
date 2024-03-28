8a2c36a5534144c761c80ed125c65a9513b594bc
arthur <arthurfleck8@proton.me>
2024-03-28 09:34:48 +0100

    basic default config

---
+++ xmonad/.config/xmonad/xmonad.hs
@@ -0,0 +1,9 @@
+import XMonad
+
+import XMonad.Util.EZConfig
+
+-- a basic configuration — which is the same as the default config
+-- default config: https://github.com/xmonad/xmonad/blob/master/src/XMonad/Config.hs
+main :: IO ()
+main = xmonad def
+
