01bb2e17c0e99c2ac2cc9187f0635da61fd75a42
arthur <arthurfleck8@proton.me>
2024-03-28 09:47:28 +0100

    add 3col layout

--- xmonad/.config/xmonad/xmonad.hs
+++ xmonad/.config/xmonad/xmonad.hs
@@ -1,18 +1,31 @@
 import XMonad
 
 import XMonad.Util.EZConfig
+import XMonad.Layout.ThreeColumns
 
 -- a basic configuration — which is the same as the default config
 -- default config: https://github.com/xmonad/xmonad/blob/master/src/XMonad/Config.hs
 -- Do note that it is not advised to copy that file and use it as the basis of your configuration, as you won’t notice when a default changes!
 
+
+myLayout = tiled ||| Mirror tiled ||| Full ||| threeCol
+  where
+    threeCol = ThreeColMid nmaster delta ratio
+    tiled    = Tall nmaster delta ratio
+    nmaster  = 1      -- Default number of windows in the master pane
+    ratio    = 1/2    -- Default proportion of screen occupied by master pane
+    delta    = 3/100  -- Percent of screen to increment by when resizing panes
+
+
 main :: IO ()
 main = xmonad $ def
-    { modMask = mod4Mask  -- Rebind Mod to the Super key
+    { modMask    = mod4Mask  -- Rebind Mod to the Super key
+    , layoutHook = myLayout  -- Use custom layouts
     }
   `additionalKeysP`
     [ ("M-S-z", spawn "xscreensaver-command -lock")
     , ("M-C-s", unGrab *> spawn "scrot -s"        )
     , ("M-f"  , spawn "firefox"                   )
     ]
+
 
