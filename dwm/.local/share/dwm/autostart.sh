068de68407ca4630b7e68d9084bc947e7c95871e
arthur <arthurfleck8@proton.me>
2024-04-06 20:05:51 +0200

    add dwm autostart script

---
+++ dwm/.local/share/dwm/autostart.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+#
+# why nm doesnt work when started here
+#nm-applet &
+#blueman-applet &
+
+dunst &
+.screenlayout/arandr-config-viewsonic.sh &
+sleep 2
+xfce4-power-manager &
+feh --bg-fill --randomize ~/Pictures/wallpapers &
+emacs --daemon &
+sxhkd -t 3 &
+#picom &
+$HOME/scripts/dwm-simple-status.sh &
+syncthing -no-browser &
+
+#nm-applet &
+#sleep 1
+#protonvpn-cli ks --off && protonvpn-cli ks --permanent && protonvpn-cli c --protocol tcp --fastest &
+
+#megasync &
