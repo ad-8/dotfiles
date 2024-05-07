18f6ba829dd760e8c394a6ffb6778a918a7725c2
arthur <arthurfleck8@proton.me>
2024-05-08 01:25:38 +0200

    curr state

--- fish/.config/fish/nnn.fish
+++ fish/.config/fish/nnn.fish
@@ -1,13 +1,15 @@
 # Source this file in config.fish !
 
 # Bookmarks: example from wiki -> spaces in path are ok
-export NNN_BMS=".:$HOME/.config;c:$HOME/code;D:$HOME/Documents;d:$HOME/Downloads/;h:$HOME;t:$HOME/Desktop;M:$HOME/MEGA/MEGAsync;m:/media/ax"
+export NNN_BMS=".:$HOME/.config;c:$HOME/code;D:$HOME/Documents;d:$HOME/Downloads/;h:$HOME;M:$HOME/MEGA/MEGAsync;m:/run/media/ax"
 
 # Plugins
-export NNN_PLUG='p:preview-tui;i:preview-tabbed;f:fzcd'
+export NNN_PLUG='P:preview-tui;p:preview-tabbed;F:fzcd;f:fzopen;'
 
 # Other
 alias nnn "nnn -e"
 
 set --export NNN_FIFO "/tmp/nnn.fifo"
 
+# Preview TUI Plugin uses this for text files
+export NNN_TERMINAL="alacritty"
