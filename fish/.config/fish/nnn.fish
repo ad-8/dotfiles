70a6be0e9577d8e25529f1dc721be6e454c9b70d
arthur <arthurfleck8@proton.me>
2025-07-15 21:04:33 +0200

    update path

--- fish/.config/fish/nnn.fish
+++ fish/.config/fish/nnn.fish
@@ -1,7 +1,7 @@
 # Source this file in config.fish !
 
 # Bookmarks: example from wiki -> spaces in path are ok
-export NNN_BMS=".:$HOME/.config;c:$HOME/code;D:$HOME/Documents;d:$HOME/Downloads/;h:$HOME;M:$HOME/MEGA/MEGAsync;m:/run/media/ax;p:$HOME/Pictures;s:$HOME/Sync"
+export NNN_BMS=".:$HOME/.config;c:$HOME/code;D:$HOME/Documents;d:$HOME/Downloads/;h:$HOME;M:$HOME/MEGA/MEGAsync;m:/run/media/ax;p:$HOME/Pictures;s:$HOME/sync"
 
 # Plugins
 export NNN_PLUG='P:preview-tui;p:preview-tabbed;F:fzcd;f:fzopen;'
