02bccebb849bd940e264bf39fc9cc7d3f2591c60
arthur <arthurfleck8@proton.me>
2025-08-16 13:28:48 +0200

    set up ruby in doom

--- doom/.config/doom/init.el
+++ doom/.config/doom/init.el
@@ -165,7 +165,10 @@
        ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
-       ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+
+       ; run `gem install ruby-lsp` in project dir after `rbenv` is properly configured
+       (ruby +lsp +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+
        ;;(rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;(scheme +guile)   ; a fully conniving family of lisps
