b20e91581f8db75c49549a8b83665c99e50147a9
arthur <arthurfleck8@proton.me>
2025-10-13 22:23:59 +0200

    add fzf preview fn

---
+++ fish/.config/fish/functions/rfv.fish
@@ -0,0 +1,21 @@
+function rfv --description 'ripgrep → fzf → vim: fuzzy search + open files or quickfix list'
+    set RELOAD 'reload:rg --column --color=always --smart-case {q} || :'
+
+    set OPENER 'if test $FZF_SELECT_COUNT -eq 0
+        vim {1} +{2}  # No selection. Open the current line in Vim.
+    else
+        vim +cw -q {+f}  # Build quickfix list for the selected items.
+    end'
+
+    fzf --disabled --ansi --multi \
+        --bind "start:$RELOAD" \
+        --bind "change:$RELOAD" \
+        --bind "enter:become:$OPENER" \
+        --bind "ctrl-o:execute:$OPENER" \
+        --bind "alt-a:select-all,alt-d:deselect-all,ctrl-/:toggle-preview" \
+        --delimiter : \
+        --preview 'bat --style=full --color=always --highlight-line {2} {1}' \
+        --preview-window "~4,+{2}+4/3,<80(up)" \
+        --query "$argv"
+end
+
