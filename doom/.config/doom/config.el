0793a3c95c3c7f6fc888cefdcd091df2e75ed2dc
arthur <arthurfleck8@proton.me>
2025-10-26 11:16:13 +0100

    doom: enable diff heading sizes

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -190,17 +190,17 @@
 
 
 ;; org-mode: set different heading sizes
-;(custom-theme-set-faces!
-;'doom-one
-;'(org-level-8 :inherit outline-3 :height 1.0)
-;'(org-level-7 :inherit outline-3 :height 1.0)
-;'(org-level-6 :inherit outline-3 :height 1.1)
-;'(org-level-5 :inherit outline-3 :height 1.2)
-;'(org-level-4 :inherit outline-3 :height 1.3)
-;'(org-level-3 :inherit outline-3 :height 1.4)
-;'(org-level-2 :inherit outline-2 :height 1.5)
-;'(org-level-1 :inherit outline-1 :height 1.6)
-;'(org-document-title  :height 1.8 :bold t :underline nil))
+(custom-theme-set-faces!
+'doom-nord
+'(org-level-8 :inherit outline-3 :height 1.0)
+'(org-level-7 :inherit outline-3 :height 1.0)
+'(org-level-6 :inherit outline-3 :height 1.1)
+'(org-level-5 :inherit outline-3 :height 1.2)
+'(org-level-4 :inherit outline-3 :height 1.3)
+'(org-level-3 :inherit outline-3 :height 1.4)
+'(org-level-2 :inherit outline-2 :height 1.5)
+'(org-level-1 :inherit outline-1 :height 1.6)
+'(org-document-title  :height 1.8 :bold t :underline nil))
 
 ;; TOOD find a better solution
 ;; ever since rubocop installed via gem, ruby-lsp is gone =lsp-describe-session=
