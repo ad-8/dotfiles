005e8c4a21e8d1b29a84bc45e2b05bbe382c2d90
arthur <arthurfleck8@proton.me>
2025-11-18 17:28:26 +0100

    emacs: fix err in config

--- doom/.config/doom/config.el
+++ doom/.config/doom/config.el
@@ -207,6 +207,20 @@
       (markdown-mode)
     (markdown-view-mode)))
 
+(setq org-directory "~/org/")
+
+(custom-theme-set-faces!
+'doom-nord
+'(org-level-8 :inherit outline-3 :height 1.0)
+'(org-level-7 :inherit outline-3 :height 1.0)
+'(org-level-6 :inherit outline-3 :height 1.1)
+'(org-level-5 :inherit outline-3 :height 1.2)
+'(org-level-4 :inherit outline-3 :height 1.3)
+'(org-level-3 :inherit outline-3 :height 1.4)
+'(org-level-2 :inherit outline-2 :height 1.5)
+'(org-level-1 :inherit outline-1 :height 1.6)
+'(org-document-title  :height 1.8 :bold t :underline nil))
+
 (custom-theme-set-faces!
 'gotham
 '(org-level-8 :inherit outline-3 :height 1.0)
