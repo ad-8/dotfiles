9e5ef016935a0115d172c7b2b38ac5502bb04595
arthur <arthurfleck8@proton.me>
2026-02-10 23:13:49 +0100

    create default.nix for vms

--- hosts/ax-vm/configuration.nix
+++ hosts/ax-vm/configuration.nix
@@ -8,40 +8,10 @@
 {
   imports = [
     ./hardware-configuration.nix
-    ../../nixos/configuration-core.nix
-    ../../nixos/ax-configs.nix
+    ../../nixos/vm/default.nix
   ];
-
-  configExtra.enable = false;
-  configClamav.enable = false;
-  configPrinting.enable = false;
-  configVirtman.enable = false;
 
   networking.hostName = "ax-vm";
-
-  services.qemuGuest.enable = true;
-  services.spice-vdagentd.enable = true; # enable copy and paste between host and guest
-
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
-  environment.systemPackages = with pkgs; [
-  ];
-
-  services.xserver = {
-    enable = true;
-    desktopManager = {
-      xterm.enable = false;
-      xfce.enable = true;
-    };
-  };
-  services.displayManager.defaultSession = "xfce";
 
   system.stateVersion = "25.05"; # Did you read the comment?
 }
