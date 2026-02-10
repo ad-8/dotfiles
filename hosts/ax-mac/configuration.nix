38f8b45dc2fc6f706f2f46273e0fb49a589e29a8
arthur <arthurfleck8@proton.me>
2026-02-10 20:02:06 +0100

    clean up

--- hosts/ax-mac/configuration.nix
+++ hosts/ax-mac/configuration.nix
@@ -45,15 +45,5 @@
   # laptop battery info via `upower --dump`
   services.upower.enable = true;
 
-  # TODO config e.g. tap to click
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
 }
