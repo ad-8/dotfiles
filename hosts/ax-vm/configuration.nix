642eb950fd575202639b6cc3ce9a610afeafdfad
arthur <arthurfleck8@proton.me>
2026-02-10 23:05:28 +0100

    clean up

--- hosts/ax-vm/configuration.nix
+++ hosts/ax-vm/configuration.nix
@@ -17,9 +17,7 @@
   configPrinting.enable = false;
   configVirtman.enable = false;
 
-  # TODO if needed, update the following line from /etc/nixos/configuration.nix
-  # boot.initrd.luks.devices."luks-f43e8971-1fb7-4d8c-be86-c8162a78d104".device = "/dev/disk/by-uuid/f43e8971-1fb7-4d8c-be86-c8162a78d104";
-  networking.hostName = "ax-vm"; # Define your hostname.
+  networking.hostName = "ax-vm";
 
   services.qemuGuest.enable = true;
   services.spice-vdagentd.enable = true; # enable copy and paste between host and guest
