4f4d05cffae7011e02fa16cbb8cc40e9e004de43
arthur <arthurfleck8@proton.me>
2026-02-12 23:25:42 +0100

    emacs: enable ibuffer module

--- dotfiles/doom/.config/doom/init.el
+++ dotfiles/doom/.config/doom/init.el
@@ -63,10 +63,10 @@
 
        :emacs
        (dired +dirvish +icons) ; making dired pretty [functional]
-       electric          ; smarter, keyword-based electric-indent
-       ;;ibuffer         ; interactive buffer management
-       undo              ; persistent, smarter undo for your inevitable mistakes
-       vc                ; version-control and Emacs, sitting in a tree
+       electric                ; smarter, keyword-based electric-indent
+       (ibuffer +icons)        ; interactive buffer management
+       undo                    ; persistent, smarter undo for your inevitable mistakes
+       vc                      ; version-control and Emacs, sitting in a tree
 
        :term
        ;;eshell            ; the elisp shell that works everywhere
