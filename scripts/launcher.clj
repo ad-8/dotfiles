8b6ce16956305408055fa8e13d0afbd530f783f8
arthur <arthurfleck8@proton.me>
2026-02-14 21:06:47 +0100

    scripts: format launcher to make it readable

--- scripts/launcher.clj
+++ scripts/launcher.clj
@@ -1,34 +1,43 @@
 (ns launcher
   (:require  [clojure.string :as str]
-             [clojure.edn]
-             [babashka.fs :as fs]
              [babashka.process :refer [process shell]]))
 
-;; a simple script to /somewhat/ replace keychords,
-;; which are still missing in niri
+
+;; a simple script to /somewhat/ replace keychords, which are still missing in niri
+
 
 (def cmds
-  {"8 brave" "brave"
-   "2 firefox" "firefox"
-   "4 qutebrowser" "qutebrowser"
-   "3 thunar (file manager)" "thunar"
-   "linkding bookmarks std" "bb /home/ax/x/ax_bookmarks.clj std"
+  {
+   "1 system"                    (format "/usr/bin/env bb %s sys" *file*)
+   "2 firefox"                   "firefox"
+   "3 thunar (file manager)"     "thunar"
+   "4 qutebrowser"               "qutebrowser"
+   "6 emms (music)"              "emacs --name ax-emacs-emms --eval '(ax/open-emms-layout)'"
+   "8 brave"                     "brave"
+   "9 rofi"                      (format "/usr/bin/env bb %s rofi" *file*)
    "linkding bookmarks archived" "bb /home/ax/x/ax_bookmarks.clj archived"
-   "1 system" (format "bb %s sys" *file*)
-   "9 rofi"   (format "bb %s rofi" *file*)})
+   "linkding bookmarks std"      "bb /home/ax/x/ax_bookmarks.clj std"
+   })
+
 
 (def sys
-  {"1 bluetui (TUI)" "alacritty -T ax-bluetui -e bluetui"
-   "2 wiremix (TUI)" "alacritty -T ax-wiremix -e wiremix"
-   "3 kill a process" "sh -c 'ps -u $USER -o pid,comm,args,%cpu,%mem | wmenu -i -l 10 -p Kill: | awk \"{print \\$1}\" | xargs -r kill'"
-   "4 volume control (pavu)" "pavucontrol"
-   "5 toggle bluetooth on/off" "rfkill toggle bluetooth"})
+  {
+   "1 bluetui (TUI)"             "alacritty -T ax-bluetui -e bluetui"
+   "2 wiremix (TUI)"             "alacritty -T ax-wiremix -e wiremix"
+   "3 kill a process"            "sh -c 'ps -u $USER -o pid,comm,args,%cpu,%mem | wmenu -i -l 10 -p Kill: | awk \"{print \\$1}\" | xargs -r kill'"
+   "4 volume control (pavu)"     "pavucontrol"
+   "5 toggle bluetooth on/off"   "rfkill toggle bluetooth"
+   })
+
 
 (def rofi
-  {"rofi - files" "rofi -show recursivebrowser"
-   "rofi - windows" "rofi -show window"})
+  {
+   "rofi - files"                "rofi -show recursivebrowser"
+   "rofi - windows"              "rofi -show window"
+   })
 
-(defn providers [prompt] {:rofi "rofi -dmenu -p mode-open"
+
+(defn providers [prompt] {:rofi (format "rofi -dmenu -p %s" prompt)
                           :wmenu (format "wmenu -i 
                                 -l 20 
                                 -f \"Hack Nerd Font 11\" 
@@ -40,8 +49,9 @@
                                 -s \"#99d1ce\"
                                 -p \"%s\"" prompt)})
 
+
 (defn run-it! [what prompt]
-  (let [dmenu-cmd (:wmenu (providers prompt))
+  (let [dmenu-cmd (:rofi (providers prompt))
         input (str/join "\n" (sort (keys what)))
         user-choice (-> (process {:in input :out :string} dmenu-cmd)
                         deref :out str/trim)
@@ -49,9 +59,11 @@
     (println user-choice "---" cmd-to-run)
     (shell cmd-to-run)))
 
+
 (let [arg (first *command-line-args*)]
   (case arg
     "main" (run-it! cmds "          Select main            ")
     "sys"  (run-it! sys  "          Select sys             ")
     "rofi" (run-it! rofi "          Select rofi            ")
     (System/exit 1)))
+
