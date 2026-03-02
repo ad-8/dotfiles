604f3d4738b36c8083c4b895ee28aa7ae71ce634
arthur <arthurfleck8@proton.me>
2026-03-02 13:17:39 +0100

    nix: delete config for x1c

--- nixos/hosts/ax-mac/home.nix
+++ nixos/hosts/ax-mac/home.nix
@@ -1,7 +1,9 @@
-{ ... }:
+{ lib, ... }:
 
 {
   imports = [
     ../../home-manager/default.nix
   ];
+  # TODO how to change the defaults
+  configQt.enable = lib.mkForce true;
 }
--- nixos/hosts/ax-x1c/home.nix
+++
@@ -1,9 +0,0 @@
-{ lib, ... }:
-
-{
-  imports = [
-    ../../home-manager/default.nix
-  ];
-  # TODO how to change the defaults
-  configQt.enable = lib.mkForce true;
-}
