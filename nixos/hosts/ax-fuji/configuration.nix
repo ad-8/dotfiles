93a301c6a23c0f87a5988489ade56d13ba906d7a
arthur <arthurfleck8@proton.me>
2026-03-02 12:26:33 +0100

    nix: use new immich backup script on server

--- nixos/hosts/ax-fuji/configuration.nix
+++ nixos/hosts/ax-fuji/configuration.nix
@@ -68,7 +68,7 @@
       systemCronJobs = [
         "10 3 * * *     ax     . /etc/profile; /usr/bin/env bb $HOME/x/backup/ax_srv_radicale.clj >> ~/cron-radicale.log 2>&1"
         "12 3 * * *     ax     . /etc/profile; /usr/bin/env bb $HOME/x/backup/ax_srv_linkding.clj >> ~/cron-linkding.log 2>&1"
-        "15 3 * * *     ax     . /etc/profile; ruby $HOME/x/backup/ax-srv-backup-immich.rb >> ~/cron-immich.log 2>&1"
+        "15 3 * * *     ax     . /etc/profile; /usr/bin/env bb $HOME/x/backup/ax_srv_immich.clj >> ~/cron-immich.log 2>&1"
         "25 3 * * *     ax     . /etc/profile; /usr/bin/env bb $HOME/x/backup/ax_srv_rclone_b2.clj >> ~/cron-rclone-b2.log 2>&1"
         # download daily wallpaper
         "0 9,10,11 * * *     ax     . /etc/profile; nix develop ~/x --command ruby ~/x/bing_wallpaper_dl.rb >> ~/bing.log 2>&1"
