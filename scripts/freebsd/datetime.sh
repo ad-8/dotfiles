1913b1292e3a7de7512b7cbefc86acd67cb2a4bc
arthur <arthurfleck8@proton.me>
2026-03-07 10:07:02 +0100

    localize weekday in datetime script

--- scripts/freebsd/datetime.sh
+++ scripts/freebsd/datetime.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env sh
 
-dt=$(date '+%a %d.%m.')
-ti=$(date '+%H:%M')
+# split date output into weekday, date and time
+set -- $(date '+%a %d.%m. %H:%M')
 
-echo " $dt  $ti"
+wd_en=$1
+d=$2
+t=$3
+
+case "$wd_en" in
+    Mon) wd_de="Mo" ;;
+    Tue) wd_de="Di" ;;
+    Wed) wd_de="Mi" ;;
+    Thu) wd_de="Do" ;;
+    Fri) wd_de="Fr" ;;
+    Sat) wd_de="Sa" ;;
+    Sun) wd_de="So" ;;
+    *)   wd_de="$wd_en" ;;
+esac
+
+echo " $wd_de $d  $t"
+
