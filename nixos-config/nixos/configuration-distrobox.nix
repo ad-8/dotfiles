ecc37663c28f7b9a4873edb0c6eb8c9ecff8849a
arthur <arthurfleck8@proton.me>
2026-03-15 10:44:03 +0100

    nix: clean up module

--- nixos-config/nixos/configuration-distrobox.nix
+++ nixos-config/nixos/configuration-distrobox.nix
@@ -12,14 +12,11 @@
 
   config = lib.mkIf config.configDistrobox.enable {
 
-    # distrobox
     virtualisation.podman = {
       enable = true;
       dockerCompat = true;
     };
 
     environment.systemPackages = [ pkgs.distrobox ];
-    users.users.ax.extraGroups = [
-    ];
   };
 }
